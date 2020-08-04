0f7c1c393c5ad79ea575aa5cf8bc76dc83bbc5a0
Ian Culp <icculp@gmail.com>
2020-08-04 15:54:14 +0000

    4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,15 +1,15 @@
 # Task 4
 # Creates a file
 file_line { 'PasswordAuthentication':
-  path              => '~/.ssh/ssh_config',
+  path    => '~/.ssh/ssh_config',
   replace => true,
-  line => 'PasswordAuthentication no',
-  match => 'PasswordAuthentication'
+  line    => 'PasswordAuthentication no',
+  match   => 'PasswordAuthentication'
 }
 
 file_line { 'IdentityFile':
     path    => '~/.ssh/ssh_config',
     replace => true,
-    line       => 'IdentityFile ~/.ssh/holberton',
-    match  => 'IdentityFile',
+    line    => 'IdentityFile ~/.ssh/holberton',
+    match   => 'IdentityFile',
   }
