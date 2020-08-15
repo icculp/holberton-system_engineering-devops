bb2f29babdaf9f5148ca70bce820dbebbf8c04e7
Ian Culp <icculp@gmail.com>
2020-08-15 03:27:42 +0000

    Wtf

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -31,6 +31,15 @@ $s = "server {
                 internal;
         }
 	root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
 }"
 
 file {'/etc/nginx/sites-available/default':
