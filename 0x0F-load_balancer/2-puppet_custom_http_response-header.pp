bdbbb15b11d29e0c52ca9660ac080f1c38799e45
Ian Culp <icculp@gmail.com>
2020-08-15 01:54:25 +0000

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,2 +1,30 @@
-package {'Package[haproxy]':
-  name => 
+exec {'apt-update':
+    command => '/usr/bin/apt-get update',
+}
+
+package {'nginx':
+  ensure => latest,
+}
+
+file {'/var/www/html/index.html':
+  ensure  => present,
+  content => 'Holberton School',
+}
+
+$s = "server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        error_page 404 /custom_404.html
+        location = /custom_404.html {
+                root /var/www/error;
+                internal;
+                add_header X-Served-By \$hostname;
+        root /var/www/html
+}"
+
+file {'/etc/nginx/sites-available/default':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  content => '$s',
+}
