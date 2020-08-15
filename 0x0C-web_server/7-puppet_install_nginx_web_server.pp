c37545b160bfd5aaeb018e4882c4bb58e0111d45
Ian Culp <icculp@gmail.com>
2020-08-15 16:17:58 +0000

    7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,26 +1,56 @@
-# Task 6
-# Install Nginx
-????
-include nginx???
+# Task 2
+# comment
 
+$s = "server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        error_page 404 /custom_404.html;
+        location = /custom_404.html {
+                root /var/www/html;
+                internal;
+        }
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
 
+        server_name _;
 
-class {'nginx':
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
+}"
 
+exec {'apt-update':
+    command => '/usr/bin/env apt-get -y update',
+}
+
+-> package {'nginx':
+  ensure   => 'installed',
+  provider => 'apt',
+}
+
+-> file {'/var/www/html/index.html':
+  ensure  => 'file',
+  path => '/var/www/html/index.html',
+  content => "Holberton School\n",
+}
+
+-> file {'custom_404.html':
+  ensure  => 'file',
+  path => '/var/www/html/custom_404.html',
+  content => "Ceci n'est pas une page\n",
 }
 
 
-server {
-  listen 80;
+-> file {'/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  path    => '/etc/nginx/sites-available/default',
+  content => "${s} ",
 }
 
-
-
-
-
-nginx::resource::location {"www.example.org-wiki":
-  ensure             => present,
-  vhost              => 'www.example.org',
-  location           => '/wiki',
-  redirect           => 'http://wiki.example.org'
+-> service {'nginx':
+  ensure  => 'running',
+  restart => 'sudo service nginx restart',
 }
