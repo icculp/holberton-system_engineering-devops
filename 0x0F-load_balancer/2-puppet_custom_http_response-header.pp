7c056323751b66f5a4544fac4b853262d301dc0d
Ian Culp <icculp@gmail.com>
2020-08-15 02:36:58 +0000

    Trying again

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -11,15 +11,30 @@ file {'/var/www/html/index.html':
   content => 'Holberton School',
 }
 
+file {'/var/www/error/custom_404.html':
+  ensure  => present,
+  content => "Ceci n'est pas une page\n",
+}
+
 $s = "server {
         listen 80 default_server;
         listen [::]:80 default_server;
         rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
         add_header X-Served-By ${hostname};
+        error_page 404 /custom_404.html;
+        location = /custom_404.html {
+                root /var/www/error;
+                internal;
+        }
 }"
 
 file {'/etc/nginx/sites-available/default':
   ensure  => present,
   path    => '/etc/nginx/sites-available/default',
   content => "${s} ",
+}
+
+service {'nginx':
+  ensure  => running,
+  restart => 'sudo service nginx restart',
 }
