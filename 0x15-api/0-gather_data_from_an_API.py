1dd8e16810b3985a47c9750f4ebd14b40d3f01a3
Ian Culp <icculp@gmail.com>
2020-09-14 20:52:46 +0000

    name guard

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,21 +6,22 @@ import requests
 from sys import argv
 
 
-rusr = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                    .format(argv[1]))
-rtodo = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
-                     .format(argv[1]))
-jusr = rusr.json()
-jtodo = rtodo.json()
+if __name__ == '__main__':
+    rusr = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(argv[1]))
+    rtodo = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                         .format(argv[1]))
+    jusr = rusr.json()
+    jtodo = rtodo.json()
 
-done = len(jtodo)
-notdone = 0
-name = jusr.get('name')
-for t in jtodo:
-    if t.get('completed') is True:
-        notdone = notdone + 1
+    done = len(jtodo)
+    notdone = 0
+    name = jusr.get('name')
+    for t in jtodo:
+        if t.get('completed') is True:
+            notdone = notdone + 1
 
-print("Employee {} is done with tasks({}/{}):".format(name, notdone, done))
-for t in jtodo:
-    if t.get('completed') is True:
-        print("\t {}".format(t.get('title')))
+    print("Employee {} is done with tasks({}/{}):".format(name, notdone, done))
+    for t in jtodo:
+        if t.get('completed') is True:
+            print("\t {}".format(t.get('title')))
