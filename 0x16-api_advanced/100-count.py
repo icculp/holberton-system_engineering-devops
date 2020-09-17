472ff9f14b18b72e4c0a672ee2272ec9bda1183a
Ian Culp <icculp@gmail.com>
2020-09-17 23:01:29 +0000

    3

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Task 3
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ gets the number of subscribers """
+    if after is not None:
+        url = 'https://api.reddit.com/r/' +\
+              '{}/hot.json?after={}'.format(subreddit, after)
+    else:
+        url = 'https://api.reddit.com/r/{}/hot.json'.format(subreddit)
+    header = {'user-agent': 'ianscustomthing'}
+
+    r = requests.get(url,
+                     headers=header,
+                     allow_redirects=False)
+    rj = r.json()
+    if rj.get('message') == 'Not Found':
+        return
+    chldrn = rj.get('data').get('children')
+    hot_list += [chld.get('data').get('title') for chld in chldrn]
+    after = rj.get('data').get('after')
+    if after is not None:
+        return recurse(subreddit, hot_list, after)
+    else:
+        return hot_list
