310fec9a23e4bb01b923f7639237ea0056ceb872
Ian Culp <icculp@gmail.com>
2020-09-17 17:36:06 +0000

    Adding 0x16

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Task 0
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ gets the number of subscribers """
+    r = requests.get('https://api.reddit.com/r/{}/about.json'
+                     .format(subreddit),
+                     headers={'user-agent': 'ianscustomthing'},
+                     allow_redirects=False)
+    rj = r.json()
+    if rj.get('message') == 'Not Found':
+        return 0
+    s = rj.get('data').get('subscribers')
+    return s
