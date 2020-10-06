20087f6c1fe1261b5cc715379a99378a0fd64050
Ian Culp <icculp@gmail.com>
2020-10-06 22:25:25 +0000

    Adding 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+file {'/etc/default/nginx':
+  ensure  => present,
+  path    =>  '/etc/default/nginx',
+  content => 'ULIMIT="-n 4096"',
+}
+
+-> exec {'nginx':
+  command => 'sudo service nginx restart',
+  path    => ['/bin', '/usr/bin'],
+}
