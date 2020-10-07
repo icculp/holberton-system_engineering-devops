99f1b8dc3fe975dfb679bc5b41772b6c05f7166f
Ian Culp <icculp@gmail.com>
2020-10-07 02:31:57 +0000

    Adding 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+exec {'limits':
+  command => "sudo sed -i 's,holberton hard nofile.*,holberton hard nofile 5000,' /etc/security/limits.conf",
+  path    => ['/bin', '/usr/bin'],
+}
+exec {'limits.conf':
+  command => "sudo sed -i 's,holberton soft nofile.*,holberton soft nofile 5000,' /etc/security/limits.conf",
+  path    => ['/bin', '/usr/bin'],
+}
